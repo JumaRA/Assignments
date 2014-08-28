fde863109a489cb366070b99afce49a8fdc849ff
JumaRA <rebecca@aims.edu.gh>
2014-08-28 13:45:38 +0000

    new file

--- HW.py
+++ HW.py
@@ -3,3 +3,29 @@ print("hallo ghana")
 print("hallo world")
 print("this is biriwa")
 print("i love travelling and visiting new places")
+
+from numpy import*
+print(pi)
+
+r=4.6595959
+print(type(r))
+
+for i in range(10):
+    print(i)
+
+def area(l,w):
+    x=l*w
+    print(x)
+l = 4
+w= 5
+area(4,5)
+
+from numpy import*
+def area(r):
+    x=((pi)*r*r)
+    print(x)
+r=10
+area(10)
+
+
+
