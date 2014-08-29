3258cdac963aefd5fedfa678810fad26ee5144a8
JumaRA <rebecca@aims.edu.gh>
2014-08-29 08:14:06 +0000

    geometric formulae

--- geom_formulae.py
+++ geom_formulae.py
@@ -49,4 +49,47 @@ def Volume_Cylinder(radius,height: Number) -> Number:
     """
     return (pi*radius*radius*height)
 
+def Cube_Volume(side: Number) -> Number:
+    """
+    calculates the volume of a cube given the side, all sides are equal
+    :param side: the length of the side of the cube
+    :return:the volume of the cube
+    >>>Cube_Volume(2)
+    8
+    """
+    return side*side*side
+
+def Cube_Area(side: Number) -> Number:
+    """
+    calculates the area of a cube given the length of any side, all sides are equal
+    :param side: length of the side
+    :return:the surface area of the cube
+    >>>Cube_Volume(4)
+    96
+    """
+    return 6*side*side
+
+from numbers import Number
+from numpy import*
+def Sphere_Area(radius: Number) -> Number:
+    """
+    calculates the surface area of a sphere with a given radius
+    :param radius: radius of the sphere
+    :return:the surface area of the sphere
+    >>>Sphere_Area(3)
+    113.097334
+    """
+    return (4*pi*radius*radius)
+from numbers import Number
+from numpy import*
+def Cone_Volume(radius,height: Number) -> Number:
+    """
+    calculates the volume of cone with a given radius and height
+    :param radius: base radius of the cone
+    :param height: height of the cone
+    :return:the volume of the cone
+    >>>Cone_Volume(7,2)
+    102.625
+    """
+    return (1/3)*pi*radius*radius*height
 
