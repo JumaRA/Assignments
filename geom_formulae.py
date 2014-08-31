e9c7bbca26224c5d3437d729ce8e5bcb6a1e720f
JumaRA <rebecca@aims.edu.gh>
2014-08-31 04:13:40 +0000

    final corrections

--- geom_formulae.py
+++ geom_formulae.py
@@ -4,9 +4,9 @@ from numbers import Number
 def area_rectangle(length,width: Number) -> Number:
     """
     calculates the area of a rectangle with given length and width
-    :param length: the side length
-    :param width: the side width
-    :return:the area of a rectangle
+    @param length: the side length
+    @param width: the side width
+    @return:the area of a rectangle
     >>>area_rectangle(4,8)
     32
     """
@@ -16,8 +16,8 @@ from numpy import*
 def Area_Circle(radius: Number) -> Number:
     """
     calculates the area of a circle with a given radius
-    :param radius: radius of the circle
-    :return:area of the circle
+    @param radius: radius of the circle
+    @return:area of the circle
     >>> Area_Circle(10)
     314.15926
     """
@@ -27,8 +27,8 @@ def Area_Circle(radius: Number) -> Number:
 def Circle_Perimeter(radius: Number) -> Number:
     """
     calculates the circumference of a circle with a given radius
-    :param radius: radius of the circle
-    :return:Perimeter of the circle
+    @param radius: radius of the circle
+    @return:Perimeter of the circle
     >>> Circle_Perimeter(10)
     62.831853
     """
@@ -37,9 +37,9 @@ def Circle_Perimeter(radius: Number) -> Number:
 def Volume_Cylinder(radius,height: Number) -> Number:
     """
     Calculates the volume of a cylinder with a given radius and height
-    :param radius: base radius of cylinder
-    :param height: height of the cylinder
-    :return:the volume of the cylinder
+    @param radius: base radius of cylinder
+    @param height: height of the cylinder
+    @return:the volume of the cylinder
     >>>Volume_Cylinder(7,1)
     153.93804
     """
@@ -48,8 +48,8 @@ def Volume_Cylinder(radius,height: Number) -> Number:
 def Cube_Volume(side: Number) -> Number:
     """
     calculates the volume of a cube given the side, all sides are equal
-    :param side: the length of the side of the cube
-    :return:the volume of the cube
+    @param side: the length of the side of the cube
+    @return:the volume of the cube
     >>>Cube_Volume(2)
     8
     """
@@ -58,8 +58,8 @@ def Cube_Volume(side: Number) -> Number:
 def Cube_Area(side: Number) -> Number:
     """
     calculates the area of a cube given the length of any side, all sides are equal
-    :param side: length of the side
-    :return:the surface area of the cube
+    @param side: length of the side
+    @return:the surface area of the cube
     >>>Cube_Volume(4)
     96
     """
@@ -68,8 +68,8 @@ def Cube_Area(side: Number) -> Number:
 def Sphere_Area(radius: Number) -> Number:
     """
     calculates the surface area of a sphere with a given radius
-    :param radius: radius of the sphere
-    :return:the surface area of the sphere
+    @param radius: radius of the sphere
+    @return:the surface area of the sphere
     >>>Sphere_Area(3)
     113.097334
     """
@@ -79,9 +79,9 @@ def Sphere_Area(radius: Number) -> Number:
 def Cone_Volume(radius,height: Number) -> Number:
     """
     calculates the volume of cone with a given radius and height
-    :param radius: base radius of the cone
-    :param height: height of the cone
-    :return:the volume of the cone
+    @param radius: base radius of the cone
+    @param height: height of the cone
+    @return:the volume of the cone
     >>>Cone_Volume(7,2)
     102.625
     """
@@ -90,10 +90,10 @@ def Cone_Volume(radius,height: Number) -> Number:
 def Cuboid_Volume(length,width,height: Number) -> Number:
     """
     calculates the volume of cube given the length, width and radius
-    :param length: the base length
-    :param width: the base width
-    :param height: height of the cuboid
-    :return:the volume of the cuboid
+    @param length: the base length
+    @param width: the base width
+    @param height: height of the cuboid
+    @return:the volume of the cuboid
     >>>Cuboid_Volume(2,3,4)
     """
     return length*width*height
@@ -101,10 +101,10 @@ def Cuboid_Volume(length,width,height: Number) -> Number:
 def Trapezium_Area(length1,length2,height: Number) -> Number:
     """
     calculates the area of a trapezium using a given length of the parallel sides and the height
-    :param length1: length of the shorter parallel side
-    :param length2: length of the longer parallel side
-    :param height: height of the trapezium
-    :return:the area of the trapezium
+    @param length1: length of the shorter parallel side
+    @param length2: length of the longer parallel side
+    @param height: height of the trapezium
+    @return:the area of the trapezium
     >>>Trapezium_Area(2,4,6)
     18
     """
@@ -113,8 +113,8 @@ def Trapezium_Area(length1,length2,height: Number) -> Number:
 def Hexagon_Perimeter(side: Number) -> Number:
     """
     calculates the perimeter of a regular hexagon, the length of the six sides are all equal
-    :param side: length of the side of the hexagon
-    :return:perimeter of the hexagon
+    @param side: length of the side of the hexagon
+    @return:perimeter of the hexagon
     >>>Hexagon_Perimeter(10)
     60
     """
@@ -123,11 +123,10 @@ def Hexagon_Perimeter(side: Number) -> Number:
 def triangle_area(base,height: Number) -> Number:
     """
     calculates the area of a triangle using a given base and height
-    :param base: the base length of the triangle
-    :param height: height of the triangle
-    :return:the area of the triangle
+    @param base: the base length of the triangle
+    @param height: height of the triangle
+    @return:the area of the triangle
     >>>triangle_area(2,9)
     9
     """
     return (1/2)*base*height
-
